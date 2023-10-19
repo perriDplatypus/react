b489cbd2f20393e47b20272151a5166e84e667e8
abhay <abhaytyagi2513@gmail.com>
2023-10-19 21:40:42 +1100

    added state and API call

--- module-1/src/App.js
+++ module-1/src/App.js
@@ -1,8 +1,18 @@
+import { useState } from "react";
+
 export default function App() {
-	return(
+	const [advice, setAdvice] = useState("");
+
+	async function getAdvice() {
+		const res = await fetch("https://api.adviceslip.com/advice");
+		const data = await res.json();
+		setAdvice(data.slip.advice);
+	}
+
+	return (
 		<div>
-			<h1>Hello World</h1>
-			<button>Get Advice</button>
+			<h1>{advice}</h1>
+			<button onClick={getAdvice}>Get Advice</button>
 		</div>
 	);
 }
