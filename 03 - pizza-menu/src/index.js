2cf8d96e5f1e7290d204080878c0917f9b38e22d
abhay <abhaytyagi2513@gmail.com>
2023-10-23 18:54:35 +1100

    made dynamic components

--- 03 - pizza-menu/src/index.js
+++ 03 - pizza-menu/src/index.js
@@ -69,13 +69,34 @@ const Menu = () => {
 	return (
 		<div className="menu">
 			<h2>Our Menu</h2>
-			<Pizza />
-			<Pizza />
-			<Pizza />
-			<Pizza />
+			<Pizza
+				name="Pizza Spinaci"
+				ingredients="Tomato, mozarella, spinach, and ricotta cheese"
+				photoName="pizzas/spinaci.jpg"
+				price={10}
+			/>
+			<Pizza
+				name="Pizza Funghi"
+				ingredients="Tomato, mozarella and mushrooms"
+				photoName="pizzas/funghi.jpg"
+				price={12}
+			/>
 		</div>
 	);
 };
+
+function Pizza(props) {
+	return (
+		<div className="pizza">
+			<img src={props.photoName} alt="Pizza Spinaci" />
+			<div>
+				<h3>{props.name}</h3>
+				<p>{props.ingredients}</p>
+				<span>{props.price}</span>
+			</div>
+		</div>
+	);
+}
 
 const Footer = () => {
 	const hour = new Date().getHours();
@@ -90,16 +111,6 @@ const Footer = () => {
 		</footer>
 	);
 };
-
-function Pizza() {
-	return (
-		<div>
-			<img src="pizzas/spinaci.jpg" alt="Pizza Spinaci" />
-			<h3>Pizza Spinaci</h3>
-			<p>Tomato, mozarella, spinach, and ricotta cheese</p>
-		</div>
-	);
-}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
